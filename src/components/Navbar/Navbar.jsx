1209bbbe39b31c2c0dbf05ea7bf5d2eac80b7bfe
wafas-repo <wafa.qazi@outlook.com>
2024-07-12 01:11:41 -0400

    handle window resize effect

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,64 +1,69 @@
-import { Wrapper, Content } from "./Navbar.styles"
-import { useState } from "react"
+import { useEffect, useState, useContext } from "react";
+import { Wrapper, Content } from "./Navbar.styles";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
 import { ThemeContext } from "../../Contexts/theme";
-import { useContext } from "react";
 import { FaSun } from "react-icons/fa";
 import { FaMoon } from "react-icons/fa";
 
-
-
 const Navbar = () => {
-  const [showNavList, setShowNavList] = useState(false)
+  const [showNavList, setShowNavList] = useState(false);
   const { themeName, toggleTheme } = useContext(ThemeContext);
 
-  const toggleNavList = () => setShowNavList(!showNavList)
+  const toggleNavList = () => setShowNavList(!showNavList);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 600) {
+        setShowNavList(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <Wrapper>
       <Content>
         <nav className="centre">
-            <ul className={`nav__list ${showNavList ? 'display-nav-list' : ''}`}>
-                <li className="nav__list-item">
-                    <a className="link link--nav" href="#about">
-                        about
-                    </a>
-                </li>
-                <li className="nav__list-item">
-                  <a className="link link--nav" href="#projects">
-                  projects
-                    </a>
-                  
-                </li>
-                <li className="nav__list-item">
-                  <a className="link link--nav" href="#contact">
-                    contact
-                  </a>
-                  
-                </li>
-            </ul>
-            <button
-              type="button"
-                onClick={toggleTheme}
-                className='btn btn--icon nav__theme'
-                aria-label='toggle theme'
-            >
-              {themeName === 'dark' ? <FaSun /> : <FaMoon />}
-            </button>
-            <button
-              type="button"
-              onClick={toggleNavList}
-              className="btn btn--icon nav__hamburger"
-              aria-label='toggle navigation'
-            >
-              {showNavList ? <IoMdClose /> : <GiHamburgerMenu />}
-            </button>
-            
+          <ul className={`nav__list ${showNavList ? "display-nav-list" : ""}`}>
+            <li className="nav__list-item">
+              <a className="link link--nav" href="#about">
+                about
+              </a>
+            </li>
+            <li className="nav__list-item">
+              <a className="link link--nav" href="#projects">
+                projects
+              </a>
+            </li>
+            <li className="nav__list-item">
+              <a className="link link--nav" href="#contact">
+                contact
+              </a>
+            </li>
+          </ul>
+          <button
+            type="button"
+            onClick={toggleTheme}
+            className="btn btn--icon nav__theme"
+            aria-label="toggle theme"
+          >
+            {themeName === "dark" ? <FaSun /> : <FaMoon />}
+          </button>
+          <button
+            type="button"
+            onClick={toggleNavList}
+            className="btn btn--icon nav__hamburger"
+            aria-label="toggle navigation"
+          >
+            {showNavList ? <IoMdClose /> : <GiHamburgerMenu />}
+          </button>
         </nav>
       </Content>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Navbar/Navbar.styles.js
+++ src/components/Navbar/Navbar.styles.js
@@ -1,81 +1,76 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
- 
 `;
 
 export const Content = styled.div`
+  display: flex;
+  align-items: center;
 
+  ul {
+    list-style-type: none;
+  }
+  .centre {
     display: flex;
     align-items: center;
+  }
 
-    ul {
-        list-style-type: none;
-    }
-    .centre {
-        display: flex;
-        align-items: center;
-    }
+  .nav__hamburger {
+    display: none;
+  }
 
-    .nav__hamburger {
-        display: none;
-    }
+  .nav__list {
+    margin-right: 1.5em;
+    display: flex;
+  }
 
+  .nav__list-item {
+    margin-left: 1.5em;
+  }
+
+  .link--nav {
+    color: var(--clr-fg);
+    text-transform: lowercase;
+    font-weight: 500;
+  }
+
+  .nav__theme {
+    margin-top: 0.4em;
+  }
+
+  .btn--icon {
+    padding: 0;
+    font-size: 1.2rem;
+  }
+
+  @media (max-width: 600px) {
     .nav__list {
-        margin-right: 1.5em;
-        display: none;
+      display: none;
+      flex-direction: column;
+      align-items: center;
+      padding: 4em 0;
+      position: absolute;
+      right: 0;
+      left: 0;
+      top: 5em;
+      background-color: var(--clr-bg);
+      width: 0;
+      overflow: hidden;
+      transition: width 0.2s ease-in-out;
+    }
+
+    .display-nav-list {
+      display: flex;
+      width: 100%;
     }
 
     .nav__list-item {
-        margin-left: 1.5em;
+      margin: 0.5em 0;
     }
 
-    .link--nav {
-        color: var(--clr-fg);
-        text-transform: lowercase;
-        font-weight: 500;
+    .nav__hamburger {
+      display: flex;
+      margin-left: 0.8em;
     }
-
-    .nav__theme {
-        margin-top: 0.4em;
-    }
-
-    .btn--icon {
-        padding: 0;
-        font-size: 1.2rem;
-    }
-
-    @media (max-width: 600px) {
-
-        .nav__list {
-            display: flex !important;
-            flex-direction: column;
-            align-items: center;
-            padding: 4em 0;
-            position: absolute;
-            right: 0;
-            left: 0;
-            top: 5em;
-            background-color: var(--clr-bg);
-            width: 0;
-            overflow: hidden;
-            transition: width 0.2s ease-in-out;
-        }
-
-        .display-nav-list {
-            width: 100%;
-        }
-        
-
-        .nav__list-item {
-            margin: 0.5em 0;
-        }
-
-        .nav__hamburger {
-            display: flex;
-            margin-left: 0.8em;
-        }
-
-}
-
+  }
 `;
